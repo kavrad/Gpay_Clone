f4ed9be30babf3aae8ec7a7bc7723f907b267dc7
kavrad <kavyaht39@gmail.com>
2023-12-05 22:42:10 +0530

    Define user model

---
+++ models/User.js
@@ -0,0 +1,50 @@
+//import mongoose
+const mongoose=require("mongoose");
+
+//define user schema
+const usersSchema=new mongoose.Schema({
+  name:{
+    type:String,
+    required:true,
+    trim:true
+  },
+  phoneNumber:{
+    type:String,
+    required:true,
+    unique:true,
+    maxLength:10
+  },
+  email:{
+    type:String,
+    required:true
+  },
+  password:{
+    type:String,
+    required:true,
+    minLength:6,
+    
+  },
+  loggedFirstTime:{
+    type:Boolean,
+    default:false
+  },
+  wallet:{
+    type:Number,
+    default:0.0,
+  },
+  sender:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User"
+  }],
+  reciever:[
+    {
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"User"
+    }
+  ]
+})
+
+
+
+
+module.exports=mongoose.model("User",usersSchema);
